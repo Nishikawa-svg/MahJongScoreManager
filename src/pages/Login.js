f5b0fd89f3511427e927035ef252449861d78d5e
nishikawa <nishikawa@sample.com>
2021-04-11 23:24:10 +0900

    styled login and signup pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,48 @@
 import React, { useContext, useState } from "react";
-import { Button, TextField } from "@material-ui/core";
+import { Button, TextField, Paper, Grid, makeStyles } from "@material-ui/core";
 import { AuthContext } from "../contexts/AuthContext";
 import { Link } from "react-router-dom";
+import LockIcon from "@material-ui/icons/Lock";
+
+const useStyles = makeStyles((theme) => ({
+  formPaper: {
+    marginTop: 20,
+    width: "100%",
+    textAlign: "center",
+    border: "solid 1px",
+    marginBottom: 30,
+  },
+  formTitle: {
+    fontSize: 24,
+    margin: "8px 8px",
+  },
+  formLockIcon: {
+    width: 40,
+    height: 40,
+    marginTop: 20,
+  },
+  formInputBox: {
+    height: 40,
+    marginBottom: 30,
+  },
+  formButton: {
+    height: 50,
+    marginBottom: 10,
+    backgroundColor: "#1870db",
+    color: "white",
+    textTransform: "none",
+    "&:hover": {
+      backgroundColor: "blue",
+    },
+  },
+  linkContanier: {
+    marginBottom: 5,
+  },
+  signupLink: {
+    textDecoration: "underline",
+    color: "blue",
+  },
+}));
 
 const initialFormInput = {
   email: "",
@@ -14,30 +55,76 @@ const Login = () => {
   const handleLogin = () => {
     login(formInput.email, formInput.password);
   };
+  const classes = useStyles();
 
   return (
     <>
-      <h1>Login form</h1>
-      <div>
-        email :{" "}
-        <TextField
-          value={formInput.communityId}
-          onChange={(e) =>
-            setFormInput({ ...formInput, email: e.target.value })
-          }
-        />
-      </div>
-      <div>
-        Password :{" "}
-        <TextField
-          value={formInput.password}
-          onChange={(e) =>
-            setFormInput({ ...formInput, password: e.target.value })
-          }
-        />
-      </div>
-      <Button onClick={handleLogin}>send</Button>
-      <Link to="/signup">Go to sign up</Link>
+      <Grid container justify="center">
+        <Grid item xs={10} sm={5} md={4} lg={3}>
+          <Paper className={classes.formPaper}>
+            <Grid container justify="center">
+              <Grid item xs={10}>
+                <Grid container direction="column">
+                  <Grid item>
+                    <LockIcon
+                      color="secondary"
+                      className={classes.formLockIcon}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <div className={classes.formTitle}>Sign In</div>
+                  </Grid>
+                  <Grid container justify="flex-start">
+                    <Grid item>Email</Grid>
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      className={classes.formInputBox}
+                      variant="outlined"
+                      fullWidth
+                      value={formInput.email}
+                      onChange={(e) =>
+                        setFormInput({ ...formInput, email: e.target.value })
+                      }
+                    />
+                  </Grid>
+                  <Grid container justify="flex-start">
+                    <Grid item>Password</Grid>
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      className={classes.formInputBox}
+                      variant="outlined"
+                      fullWidth
+                      value={formInput.password}
+                      onChange={(e) =>
+                        setFormInput({ ...formInput, password: e.target.value })
+                      }
+                    />
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      className={classes.formButton}
+                      fullWidth
+                      variant="contained"
+                      onClick={handleLogin}
+                    >
+                      Sign In
+                    </Button>
+                  </Grid>
+                  <Grid container justify="flex-end">
+                    <Grid item className={classes.linkContanier}>
+                      <Link to="/signup" className={classes.signupLink}>
+                        Go to sign up
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
     </>
   );
 };
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,48 +1,151 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import { Button, TextField } from "@material-ui/core";
+import { Button, TextField, Paper, Grid, makeStyles } from "@material-ui/core";
+import PersonAddIcon from "@material-ui/icons/PersonAdd";
 import { AuthContext } from "../contexts/AuthContext";
+
+const useStyles = makeStyles((theme) => ({
+  formPaper: {
+    marginTop: 20,
+    width: "100%",
+    textAlign: "center",
+    border: "solid 1px",
+    marginBottom: 30,
+  },
+  formTitle: {
+    fontSize: 24,
+    margin: "8px 8px",
+  },
+  formIcon: {
+    width: 40,
+    height: 40,
+    marginTop: 20,
+  },
+  formInputBox: {
+    height: 40,
+    marginBottom: 30,
+  },
+  formButton: {
+    height: 50,
+    marginBottom: 10,
+    backgroundColor: "#1870db",
+    color: "white",
+    textTransform: "none",
+    "&:hover": {
+      backgroundColor: "blue",
+    },
+    linkContanier: {
+      marginBottom: 5,
+    },
+    signinLink: {
+      textDecoration: "underline",
+      color: "blue",
+    },
+  },
+}));
 
 const SignUp = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordComfirmation, setPasswordConfirmation] = useState("");
   const { signUp } = useContext(AuthContext);
+  const classes = useStyles();
   const handleSignUp = () => {
-    signUp(username, email, password);
+    if (password !== passwordComfirmation) {
+      alert("password and confirm password should be matched");
+    } else {
+      signUp(username, email, password);
+    }
   };
   return (
     <>
-      <Link to="/">go to log in page</Link>
-      <div>Sign Up</div>
-      <br />
-      <div>
-        username :{" "}
-        <TextField
-          variant="outlined"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        email :{" "}
-        <TextField
-          type="email"
-          variant="outlined"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        password :{" "}
-        <TextField
-          variant="outlined"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-
-      <Button onClick={handleSignUp}>Sign Up</Button>
+      <Grid container justify="center">
+        <Grid item xs={10} sm={5} mg={4} lg={3}>
+          <Paper className={classes.formPaper}>
+            <Grid container justify="center">
+              <Grid item xs={10}>
+                <Grid container direction="column">
+                  <Grid item>
+                    <PersonAddIcon
+                      color="secondary"
+                      className={classes.formIcon}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <div className={classes.formTitle}>Sign Up</div>
+                  </Grid>
+                  <Grid container justify="flex-start">
+                    <Grid item>Username</Grid>
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      className={classes.formInputBox}
+                      variant="outlined"
+                      fullWidth
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid container justify="flex-start">
+                    <Grid item>Email</Grid>
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      className={classes.formInputBox}
+                      variant="outlined"
+                      fullWidth
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid container justify="flex-start">
+                    <Grid item>Password</Grid>
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      className={classes.formInputBox}
+                      variant="outlined"
+                      fullWidth
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid container justify="flex-start">
+                    <Grid item>Confirm Password</Grid>
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      className={classes.formInputBox}
+                      variant="outlined"
+                      fullWidth
+                      value={passwordComfirmation}
+                      onChange={(e) => setPasswordConfirmation(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      className={classes.formButton}
+                      fullWidth
+                      variant="contained"
+                      onClick={handleSignUp}
+                    >
+                      Sign Up
+                    </Button>
+                  </Grid>
+                  <Grid container justify="flex-end">
+                    <Grid item className={classes.linkContanier}>
+                      <Link to="/" className={classes.signinLink}>
+                        Go to sign in
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
     </>
   );
 };
